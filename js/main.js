f0179113ee108cefe435f0072fddd1074339dc4b
Anvar818 <anvar7dev@gmail.com>
2022-05-09 10:46:35 +0500

    project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+  var info ={}
+
+
+  alert("Assalomu aleykum kursda qatnashish uchun mavjud bo'sh maydonlarni to'ldiring")
+  info.fullName = prompt ("Ism va Familyangizni to`liq kiriting", "John Kenny")
+  info.age = prompt ("Yoshingizni kiriting", 25)
+  info.email = prompt ("Email pochtangizni kiriting", "qwerty@gmail.com")
+  info.course = prompt ("Qaysi yo'nalishda o'qimohchisiz?", "web dastulash")
+  info.goal = prompt ("Kursda o'qishdan maqsandingiz nima? ")
+  info.why = prompt ("Nima uchun bizni kursni tanladingiz?")
+  info.future = prompt ("Kursni bitirgach qancha maoshli ishda ishlashni hohlaysiz?")
+  info.joke = prompt ("Albatta siz hohlagan oylikda ish topishingiz qiyin bo'ladi,Siz kursni\nbitirgach Junior bo'lasiz, shuning uchun haqiqatga yaqin maoshni yozing")
+  info.comment = prompt ("Taflik va shikoyatlaringizni shu yerda yozib qoldiring")
+  
+  alert("Javoblaringiz uchun sizga minnatdorchilik bildiramiz")
+
+  console.log("Ma'lumotlar", info)
